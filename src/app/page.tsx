8b5022793ce66b30bdd90149a80096c1c741347f
lalitcap23 <lalitrajput232002@gmail.com>
2025-01-26 03:33:33 +0530

    feed cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,17 @@
-import Image from "next/image";
+import { FaTwitter } from "react-icons/fa"; 
 
 export default function Home() {
   return (
-    <div className=" grid grid-cols-12 h-screen w-screen">
+    <div className="grid grid-cols-12 h-screen w-screen">
+      {/* Left Section */}
+      <div className="col-span-3 flex  justify-start p-7">
+        <FaTwitter className="text-5xl hover:bg-gray-600 rounded-full p-3 cursor-pointer transition-all " />
+      </div>
 
-   <div className="col-span-3 "></div>
-   <div className="col-span-6 border-r-2 border-l-2 border-r-slate-400"></div>
-   <div className="col-span-3"></div>
- 
+      <div className="col-span-6 border-r-[0.1px] border-l-[0.1px] border-gray-white-400 h-full"></div>
 
-  
-    </div> 
-
-      );
+      <div className="col-span-3"></div>
+    </div>
+  );
 }
+
