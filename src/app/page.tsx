ab153dfdc10c9bf1425032f07fb27b7326679261
lalitcap23 <lalitrajput232002@gmail.com>
2025-01-26 18:35:33 +0530

    changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
-import { FaTwitter } from "react-icons/fa"; 
+import { FaTwitter } from "react-icons/fa";
+interface Tweettersidebarbutton{
+title:string;
+icon: React.ReactNode;
 
+} 
+ const sidemanuItems:Twittersidebutton[]=[
+  {
+
+  }
+ ]
 export default function Home() {
   return (
-    <div className="grid grid-cols-12 h-screen w-screen">
+    <div className="grid grid-cols-12 h-screen w-screen px-56"> 
       {/* Left Section */}
-      <div className="col-span-3 flex  justify-start p-7">
-        <FaTwitter className="text-5xl hover:bg-gray-600 rounded-full p-3 cursor-pointer transition-all " />
+      <div className="col-span-3 flex  justify-start p-8">
+        <FaTwitter className="text-6xl hover:bg-gray-600 rounded-full p-3 cursor-pointer transition-all " />
       </div>
 
       <div className="col-span-6 border-r-[0.1px] border-l-[0.1px] border-gray-white-400 h-full"></div>
